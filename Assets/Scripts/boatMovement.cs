55ee453db61773b5e7e0b3b0e05ba98d5d167120
medstone <medstone@umich.edu>
2016-06-20 23:00:08 -0700

    Initial commit

---
+++ Assets/Scripts/boatMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boatMovement : MonoBehaviour {
+    public float shipSpeed;
+    public float turnSpeed;
+    Rigidbody rig;
+
+	// Use this for initialization
+	void Start () {
+        rig = gameObject.GetComponent<Rigidbody>();
+	}
+	
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rig.AddForce(transform.forward * shipSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rig.AddForce(-transform.forward * shipSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rig.AddForceAtPosition(transform.right * turnSpeed, transform.position - (transform.forward * (transform.lossyScale.z / 2)));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rig.AddForceAtPosition(-transform.right * turnSpeed, transform.position - (transform.forward * (transform.lossyScale.z / 2)));
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
---
+++ Assets/Scripts/islandSpawn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class islandSpawn : MonoBehaviour {
+    public GameObject islandPrefab;
+    public float width;
+    public float height;
+    public float islandNumber;
+
+	// Use this for initialization
+	void Start () {
+	    for(int i = 0; i < islandNumber; i++)
+        {
+            GameObject inst = Instantiate<GameObject>(islandPrefab);
+            inst.transform.position = new Vector3(Random.Range(-width / 2, width / 2), 0, Random.Range(-height / 2, height / 2));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
